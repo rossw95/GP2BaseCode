606092acf7be2afff58986a228b3d753ff599c9a
Brian McDonald <brian.mcdonald@gcu.ac.uk>
2015-09-06 10:52:14 +0100

    Basic Window Displayed

--- include/Common.h
+++ include/Common.h
@@ -3,12 +3,5 @@
 
 #include <iostream>
 #include <SDL.h>
-#include <SDL_opengl.h>
-
-#ifdef __APPLE__
-#include <OpenGL/glu.h>
-#elif WIN32
-#include <gl\GLU.h>
-#endif
 
 #endif
--- include/Graphics.h
+++
@@ -1,8 +0,0 @@
-#ifndef _GRAPHICS_H
-#define _GRAPHICS_H
-
-void initOpenGL();
-
-void setViewport( int width, int height );
-
-#endif
