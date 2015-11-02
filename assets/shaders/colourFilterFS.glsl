e6bdf0ade66f23657235f9878c4e8b57c2d22e47
Brian McDonald <brian.mcdonald@gcu.ac.uk>
2015-11-02 20:51:53 +0000

    Colour Filter

--- assets/shaders/colourFilterFS.glsl
+++ assets/shaders/colourFilterFS.glsl
@@ -4,9 +4,9 @@ out vec4 FragColor;
 in vec2 textureCoords;
 
 uniform sampler2D texture0;
-uniform mat3 colourFilter = mat3(0.393, 0.769, 0.189,
-	0.349, 0.686, 0.168,
-	0.272, 0.534, 0.131);
+uniform mat3 colourFilter = mat3(1.0, 0.0, 0.0,
+	0.0, 1.0, 0.0,
+	0.0, 0.0, 1.0);
 
 void main()
 {
