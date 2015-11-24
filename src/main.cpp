25c01216db5deaa9e5157f87c5bff1410ab0f548
Brian McDonald <brian.mcdonald@gcu.ac.uk>
2015-11-24 13:27:36 +0000

    Changed to V1

--- src/main.cpp
+++ src/main.cpp
@@ -204,7 +204,7 @@ int main(int argc, char * arg[])
 	//Request opengl 4.1 context, Core Context
 	SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
 	SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 4);
-	SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 5);
+	SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 1);
 
 	//Create a window
 	SDL_Window * window = SDL_CreateWindow(
